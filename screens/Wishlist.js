168abc32506aa8729c7ca93fd11083da50318876
SARBJOT2084 <sarbjot2084.be20@chitkara.edu.com>
2022-12-27 12:51:41 +0530

    sale section part2 added

--- screens/Wishlist.js
+++ screens/Wishlist.js
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+function Box(props) {
+  return (<View><Text>{props.name}</Text></View>);
+}
 const Wishlist = () => {
   return (
     <View>
-      <Text>Wishlist</Text>
+      <Box name="hello" />
+      <View className="h-10 bg-black w-full"></View>
     </View>
   )
 }
