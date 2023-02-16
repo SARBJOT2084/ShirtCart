aecfad2d0047037da39c5e9aae933d74a9a86273
SARBJOT2084 <sarbjot2084.be20@chitkara.edu.com>
2023-02-16 15:01:10 +0530

    Cart completed

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,30 +1,55 @@
-import { View, Text, Image, ScrollView, FlatList } from 'react-native'
+import { View, Text, Image, ScrollView, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/Feather';
 const data = [
   {
     id: 1,
+    url: 'https://m.media-amazon.com/images/I/71KVYfwW3+L._AC_UL480_FMwebp_QL65_.jpg',
     des: "Louis phillipe Tshirt",
+    size: 'XL',
+    gender: 'Men',
+    price: '4500',
   },
   {
     id: 2,
+    url: 'https://m.media-amazon.com/images/I/61JqO0+72yL._AC_UL480_FMwebp_QL65_.jpg',
     des: 'Polo Shirt',
+    size: 'L',
+    gender: 'Men',
+    price: '3800',
   },
   {
     id: 3,
-    des: "Zara Men's shirt",
+    url: 'https://m.media-amazon.com/images/I/51BKsDt0i9L._AC_UL480_FMwebp_QL65_.jpg',
+    des: "Vero Moda shirt",
+    size: 'M',
+    gender: 'Women',
+    price: '2400',
   }
 ]
-function Box({ props }) {
-  return <Text>
-    {props.des}
-  </Text>
+
+const Box = ({ props }) => {
+  return (
+    <View className="w-full h-56 mx-auto border-b-2 border-b-gray-300 flex flex-row">
+      <View className="w-1/2 h-4/5 mr-auto my-auto">
+        <Image source={{ uri: props.url }} className="w-full h-full" resizeMode="contain" />
+
+      </View>
+      <View className="w-1/2 h-4/5 ml-auto my-auto">
+        <Text className="mx-auto text-lg">{props.des}</Text>
+        <Text className="mx-auto text-xs"> Size : {props.size}</Text>
+        <Text className="mx-auto font-bold ">Rs.{props.price}</Text>
+        <TouchableOpacity className="bg-rose-500 w-4/5 rounded-lg h-11 mt-[10%] mx-auto"><Text className="m-auto  text-white">Proceed For Payment</Text></TouchableOpacity>
+        <TouchableOpacity className="bg-gray-300 w-4/5 rounded-lg h-11 mt-[10%] mx-auto"><Text className="mx-auto my-auto ">Remove&nbsp;&nbsp;<Icon name="trash" size={24} /></Text></TouchableOpacity>
+
+      </View>
+    </View>
+  );
 }
 const Cart = () => {
   return (
     <View className="h-full w-full">
-      {/**Your cart is empty or else it is has items */}
-      {/**Making  */}
+
       <View className="h-[15%] flex flex-row">
         <View className="mr-auto my-auto flex flex-row space-x-3">
           <View className="my-auto  ">
@@ -45,7 +70,7 @@ const Cart = () => {
       <View className="bg-rose-500 h-[10%] w-full">
         <Text className="m-auto text-white">Your Items</Text>
       </View>
-      <ScrollView className="h-[75%]">
+      <ScrollView className="h-[75%]" showsVerticalScrollIndicator={false}>
         <FlatList
           data={data}
           renderItem={({ item }) => <Box props={item} />}
